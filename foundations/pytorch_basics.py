f5bf6ec57ed9a5ad4ba52ca4b0f7812dbf5b4b8c
Piyush Mishra <piyushmishra464673@gmail.com>
2026-09-16 23:16:45 +0530

    Implement PyTorch basics (PyTorch 1/4)

---
+++ foundations/pytorch_basics.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn
+from torchtyping import TensorType
+
+class Solution:
+    def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
+        rows, columns = to_reshape.shape
+        return torch.round(torch.reshape(to_reshape, (rows * columns // 2, 2)), decimals=4)
+
+    def average(self, to_avg: TensorType[float]) -> TensorType[float]:
+        return torch.round(torch.mean(to_avg, dim=0), decimals=4)
+
+    def concatenate(self, cat_one: TensorType[float], cat_two: TensorType[float]) -> TensorType[float]:
+        return torch.round(torch.cat((cat_one, cat_two), dim=1), decimals=4)
+
+    def get_loss(self, prediction: TensorType[float], target: TensorType[float]) -> TensorType[float]:
+        return torch.round(torch.nn.functional.mse_loss(prediction, target), decimals=4)
